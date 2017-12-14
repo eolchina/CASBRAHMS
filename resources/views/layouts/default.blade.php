0ce7461a52199b771acc5c3cb56de77984e0ef8e
eolchina <lswang@ibcas.ac.cn>
2017-12-14 20:14:12 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>CASBRAHMS Platform</title>
+        <title>@yield('title', 'CASBRAHMS Platform')</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+        @include('layouts._header')
+
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+
+
         @yield('content')
+        @include('layouts._footer')
+        </div>
+    </div>
+
     </body>
 </html>
